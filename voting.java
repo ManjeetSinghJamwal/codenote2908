2964da785f406f74529e37239d0ec6403047c2d2
manjeet.singh.jamwal282gmail.com <manjeet.singh.jamwal28@gmail.com>
2025-08-30 14:55:45 +0530

    update_30/08

---
+++ voting.java
@@ -0,0 +1,16 @@
+public class voting{
+public static void main(String[] args){
+int age=27;
+if(age>=18){
+System.out.println("Your age is "+age+"Year:"+" You are Eligible for vote");
+
+}
+else
+{
+System.out.println("Your age is "+age+"Year:"+"Not Eligible for vote");
+}
+
+
+}
+
+}
